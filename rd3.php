1e6c72fec0b4a91c6d79dd7456f20e3f0f570cdd
Ian_Tsai <kmygrock666@gmail.com>
2016-08-22 05:47:40 +0000

    踩地雷程式

---
+++ rd3.php
@@ -0,0 +1,79 @@
+<?php
+$array[] = array();
+for ($i=0;$i<10;$i++) {
+    for ($j=0;$j<10;$j++) {
+          $array[$i][$j] = "";
+    }
+}
+
+while ($num<40) {
+    $x = rand(0,9);
+    $y = rand(0,9);
+    if ($array[$x][$y] == ""){
+        $array[$x][$y] = 'M';
+    } else {
+        $num -= 1;
+    }
+    $num += 1;
+}
+
+for ($i=0;$i<10;$i++) {
+    for ($j=0;$j<10;$j++) {
+        $count_bomb = 0;
+        if ($array[$i][$j] == 'M') {
+            continue;
+        }
+
+        //左上
+        if ($array[$i-1][$j-1] == 'M') {
+            $count_bomb += 1;
+        }
+        //正上
+        if ($array[$i-1][$j] == 'M') {
+            $count_bomb += 1;
+        }
+        //右上
+        if ($array[$i-1][$j+1] == 'M') {
+            $count_bomb += 1;
+        }
+        //左
+        if ($array[$i][$j-1] == 'M') {
+            $count_bomb += 1;
+        }
+        //右
+        if ($array[$i][$j+1] == 'M') {
+            $count_bomb += 1;
+        }
+        //左下
+        if ($array[$i+1][$j-1] == 'M') {
+            $count_bomb += 1;
+        }
+        //正下
+        if ($array[$i+1][$j] == 'M') {
+            $count_bomb += 1;
+        }
+        //右下
+        if ($array[$i+1][$j+1] == 'M') {
+            $count_bomb += 1;
+        }
+
+        $array[$i][$j] = $count_bomb;
+    }
+}
+$c = 0;
+echo "<table border = '1'>";
+foreach ($array as $a) {
+    echo "<tr>";
+    foreach ($a as $b) {
+        echo "<td>$b</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
+
+foreach ($array as $a) {
+    foreach ($a as $b) {
+        echo $b;
+    }
+
+}
